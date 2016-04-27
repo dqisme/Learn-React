fad9f30c9d11da0c0a4c17a148318c9d7bff756f
dqisme <dqisme@outlook.com>
2016-04-27 15:15:45 +0800

    [dq] #N/A add html-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpackConfig = {
   entry: "./src/index.js",
   output: {
     path: "./build/",
-    filename: "index.compiled.js"
+    filename: "bundle.js"
   },
   module: {
     loaders: [
       { test:/\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Required
+      inject: false,
+      template: 'node_modules/html-webpack-template/index.ejs',
+
+      // Optional
+      appMountId: 'app',
+      baseHref: '',
+      devServer: 3000,
+      mobile: true,
+      window: {
+      }
+
+      // and any other config options from html-webpack-plugin
+      // https://github.com/ampedandwired/html-webpack-plugin#configuration
+    })
+  ]
 };
 
 module.exports = webpackConfig;
