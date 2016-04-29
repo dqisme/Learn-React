59cc053d1d0be1c794b68a3fc260660921e1448a
dqisme <dqisme@outlook.com>
2016-04-29 19:37:56 +0800

    [dq] #N/A separate app from index

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <p>
+      Hello, World. Nice to meet you twenty-third!
+    </p>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import config from '../config';
 
-function App() {
-  return (
-    <p>
-      Hello, World. Nice to meet you twenty-third!
-    </p>
-  );
-}
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById(config.appMountId));
